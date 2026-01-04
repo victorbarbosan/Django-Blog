ff2fa8877d53e49c8d35bf281922505bec7186b8
Victor Barbosa <105869038+victorbarbosan@users.noreply.github.com>
2026-01-04 11:26:05 -0500

    add subdomain blog.victor-barbosa.com

--- my_site/settings.py
+++ my_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['victor-barbosa.com',
+ALLOWED_HOSTS = ['blog.victor-barbosa.com', 'victor-barbosa.com',
                  'www.victor-barbosa.com', 'localhost', '127.0.0.1']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
